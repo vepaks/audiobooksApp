b9981813aa804aa8f031c961ee15f68e78280c9e
Aleksandar Kamburov <kamburoff@gmail.com>
2023-07-13 16:11:13 +0300

    add post logic

--- src/app/admin/add-audiobook/add-audiobook.component.ts
+++ src/app/admin/add-audiobook/add-audiobook.component.ts
@@ -1,17 +1,33 @@
-import {Component, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-add-audiobook',
   templateUrl: './add-audiobook.component.html',
-  styleUrls: ['./add-audiobook.component.scss']
+  styleUrls: ['./add-audiobook.component.scss'],
 })
 export class AddAudiobookComponent implements OnInit {
-
   form: FormGroup | any;
   text: any;
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      text: new FormControl(null, Validators.required),
+      narrator: new FormControl(null, Validators.required),
+    });
   }
 
+  submit() {
+    if (this.form.valid) {
+      return;
+    }
+   const post: Post = {
+      title: this.form.value.title,
+      narrator: this.form.value.narrator,
+      text: this.form.value.text,
+      date: new Date(),
+    };
+  }
 }
--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -1,12 +1,20 @@
 export interface User {
-  // returnSecureToken не е задължителен аргумент
-  returnSecureToken?: boolean;
   email: string;
   password: string;
+  // returnSecureToken не е задължителен аргумент
+  returnSecureToken?: boolean;
 }
 export interface FbAuthResponse {
   // idToken не е задължителен аргумент
   idToken?: string;
   // expiresIn не е задължителен аргумент
   expiresIn?: string;
+}
+
+export interface Post {
+  id?: string,
+  title?: string,
+  text?: string,
+  narrator?: string,
+  date?: Date
 }
